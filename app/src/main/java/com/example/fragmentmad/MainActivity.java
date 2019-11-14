bdf132088dd3a50b7094ce508c3f1e8150849b32
killerqueen59 <ramadhan_tiba@apps.ipb.ac.id>
2019-11-14 20:38:25 +0700

    no message

--- app/src/main/java/com/example/fragmentmad/MainActivity.java
+++ app/src/main/java/com/example/fragmentmad/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.fragmentmad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn1, btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameFragment,new BlankFragment()).commit();
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameFragment,new BlankFragment()).commit();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameFragment,new BlankFragment2()).commit();
+            }
+        });
+
+
     }
 }
